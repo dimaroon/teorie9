6f8fbc6cc69b0e720c2d2ff59561b1cca1f509d2
dimaroon <115692397+dimaroon@users.noreply.github.com>
2022-11-01 10:01:25 +0200

    Laborator5

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll('button')
+const block = document.getElementById('block')
+const setColor=(cl)=>{
+block.className=`continut ${cl}`
+}
+
+buttons.forEach(btn =>{
+    btn.addEventListener('click', (e)=>{
+        e.preventDefault()
+        let culoare = e.target.innerText
+
+        switch(culoare){
+            case 'Galben':
+                setColor('bg-galben')
+                break;
+
+                case 'Rosu':
+                setColor('bg-rosu')
+                break;
+
+                case 'Albastru':
+                setColor('bg-albastru')
+                break;
+
+                case 'Verde':
+                setColor('bg-verde')
+                break;
+
+                case 'Sur':
+                setColor('bg-sur')
+                break;
+
+                case 'Negru':
+                setColor('bg-negru')
+                break;
+        }
+    })
+})
